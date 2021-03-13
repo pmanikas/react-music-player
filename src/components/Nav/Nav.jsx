c60c2a238bd5923997bc6a1cbe23f1854b323648
pmanikas <pmanikas.social@gmail.com>
2021-03-13 18:21:36 +0200

    Add logo and improve style

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { toggleLibrary } from "./../../store/actions/appearance.actions";
+import LazyImage from "./../LazyImage/LazyImage";
 
 import styles from "./Nav.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faStream } from "@fortawesome/free-solid-svg-icons";
 
 const Nav = () => {
+  const isLibraryOpen = useSelector(
+    (state) => state.appearanceState.isLibraryOpen
+  );
   const dispatch = useDispatch();
   const dispatchToggleLibrary = () => dispatch(toggleLibrary());
 
   return (
     <nav className={styles.nav}>
-      <h1>Chill Vibes</h1>
+      <h1 className={styles.title}>
+      <div className={styles.logo}>
+        <LazyImage src="/images/chill-vibes-logo.png" alt="Chill Vibes Logo" />
+      </div>
+        Chill Vibes</h1>
       <button
         onClick={() => dispatchToggleLibrary()}
         className={styles.navButton}
       >
-        Library &nbsp;
-        <FontAwesomeIcon icon={faMusic} />
+        {isLibraryOpen ? ('Close') : ('Library')} &nbsp;
+        <FontAwesomeIcon icon={faStream} />
       </button>
     </nav>
   );
